f251bfb06f6b6297082d3b9d1d3d69fc0bbb2562
Dongwook Kwon <tomiyadragon@gmail.com>
2021-08-16 20:04:14 +0900

    Update use_depth_camera_1.py

--- detect_with_depth/use_depth_camera_1.py
+++ detect_with_depth/use_depth_camera_1.py
@@ -32,11 +32,22 @@ try:
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_frame, alpha=0.03), cv2.COLORMAP_JET)
         #end
         color_frame_gray = cv2.cvtColor(color_frame, cv2.COLOR_BGR2GRAY)
-        
-        images = np.hstack((color_frame_gray, depth_colormap))
-        
-        cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)
-        cv2.imshow('Align Example', images)
+        color_frame_unnoise = cv2.bilateralFilter(color_frame_gray, -1, 10, 5)
+        _, color_frame_binary = cv2.threshold(color_frame_unnoise, 0, 255, cv2.THRESH_BINARY|cv2.THRESH_OTSU)
+        contours, _ = cv2.findContours(color_frame_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+#        images = np.hstack((color_frame_gray, depth_colormap))
+        for points in contours:
+            if cv2.contourArea(points) < 1000:
+                continue
+            approx = cv2.approxPolyDP(points, cv2.arcLength(points, True) * 0.02, True)
+            if len(approx) != 4:
+                continue
+            cv2.polylines(color_frame, points, True, (0, 0, 255), thickness = 3)
+            cv2.polylines(depth_colormap, points, True, (0, 0, 255), thickness = 3)
+
+
+        cv2.imshow('color', color_frame)
+        cv2.imshow('depth', depth_colormap)
         if cv2.waitKey(33) == ord('q'):
             cv2.destroyAllWindows()
             break
